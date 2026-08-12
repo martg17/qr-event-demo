58bbdec8be6522379e60be647c8e57fa0187b74e
martg17 <vovorgael@gmail.com>
2026-08-12 13:46:31 +0000

    Create data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+const participants = {
+    "001": {
+        prenom: "Jean",
+        nom: "Dupont",
+        fonction: "Participant"
+    },
+
+    "002": {
+        prenom: "Marie",
+        nom: "Kossi",
+        fonction: "Organisatrice"
+    },
+
+    "003": {
+        prenom: "Paul",
+        nom: "Mensah",
+        fonction: "Invité"
+    }
+};
